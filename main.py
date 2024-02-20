d959587d8d129aa7ba8b2c641e4a8b96bdc8ad77
willsonr8 <willsonr8@gmail.com>
2024-02-19 20:23:52 -0500

    writeSQL format correct, not necessarily working.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import csv
+import string
 
-def writeSQL(row):
+def writeSQL(row, file):
+    command = (f'INSERT INTO ToyCarOrdersAndSales VALUES (int({row[0]}), int({row[1]}), float({row[2]}), int({row[3]}), '
+               f'float({row[4]}), TO_DATE({row[5]}, \'dd/mm/yyyy\'), int({row[6]}), {row[7]}, {row[8]}, {row[9]}, '
+               f'{row[10]}, {row[11]}, {row[12]}, {row[13]}, {row[14]}, {row[15]};\n')
+    file.write(command)
 
-    return
 
 if __name__ == "__main__":
     inFile = open('Auto_Sales_data.csv')
-    outFile = open('ToyCarOrdersAndSales Insert Commands.sql', 'a')
+    outFile = open('TESTToyCarOrdersAndSales Insert Commands.sql', 'a')
     csvreader = csv.reader(inFile)
     header = next(csvreader)
     rows = []
     for row in csvreader:
-        writeSQL(row)
+        writeSQL(row, outFile)
     inFile.close()
     outFile.close()
